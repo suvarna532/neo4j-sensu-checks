04d0cbe4d66b00b7bbf3dd0d42c85f03c7511c2e
suvarna532 <49087070+suvarna532@users.noreply.github.com>
2024-03-23 16:19:54 +0530

    Added max connections as variable

--- neo4j-connections.sh
+++ neo4j-connections.sh
@@ -1,19 +1,17 @@
 #! /bin/bash
 
 HELP="
-    usage: $0 [ -w value -c value -u username -p password ]
+    usage: $0 [ -w value -c value -m value -u username -p password ]
 
         -w --> Warning value of number of connections
         -c --> Critical value number of connections
+        -m --> Maximum number of connections configuring to Neo4J database
         -u --> Neo4J Username < default username: neo4j)
         -p --> Neo4J Password < default password neo4j)
 "
 if [ "$#" -lt 2 ]; then 
     echo "${HELP}"   
 else 
-    default_username="neo4j"
-    default_password="GBRjQIX-h305bec-zO77HHw-5uBwvS9"
-
     while [ -n "${1}" ]; do
         case "${1}" in
             -w | --warning-connections)
@@ -23,6 +21,11 @@ else
                 ;;
             -c | --critical-count)
                 c_connections="${2}"
+                shift
+                shift
+                ;;
+            -m | --max-connections)
+                max_connections="${2}"
                 shift
                 shift
                 ;;
@@ -38,13 +41,7 @@ else
                 ;;
         esac
     done
-    if [ -z "${username}" ]; then
-        username="${default_username}"
-    fi
 
-    if [ -z "${password}" ]; then
-        password="${default_password}"
-    fi
     if [ -z "${c_connections}" -o -z "${w_connections}" ]; then
         status="${HELP}"
         message_string="Both critical and warning thresholds must be defined"
@@ -56,20 +53,20 @@ else
             message_string="$connection_check"
             status_code=1
         else
-            current_connections_query="$(cypher-shell -u "$username" -p "$password" "CALL dbms.listConnections()" | wc -l | awk '{print $1}')"
-            current_connections="$(echo "$current_connections_query-1" | bc -l | tr -d '\r')"
+            current_connections="$(cypher-shell -u "$username" -p "$password" "CALL dbms.listConnections()" | wc -l | awk '{print $1}')"
+            available_connections="$(echo "$max_connections-($current_connections-1)" | bc -l | tr -d '\r')"
 
-            if [ "$current_connections" -le "$c_connections" ]; then
+            if [ "$available_connections" -le "$c_connections" ]; then
                 status="CRITICAL"
-                message_string="The connections currently at a count of $current_connections have exceeded the threshold for critical"
+                message_string="Only $available_connections connections left available on Neo4J"
                 status_code=1
-            elif [ "$current_connections" -le "$w_connections" ]; then
+            elif [ "$available_connections" -le "$w_connections" ]; then
                 status="WARNING"
-                message_string="The connections currently at a count of $current_connections have exceeded the threshold for warning"
+                message_string="Only $available_connections connections left available on Neo4J"
                 status_code=2
             else
                 status="OK"
-                message_string="The connections currently are at a count of $current_connections"
+                message_string="There are $available_connections connections available on Neo4J"
                 status_code=0
             fi
         fi
